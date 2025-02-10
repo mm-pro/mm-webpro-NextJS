a366e9e128e12d762d16f1f55f9562ef8230afab
mm-pro <robert.codingpro@gmail.com>
2025-02-10 12:57:45 +0800

    define: "Outfit" and "Ovo" font in layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const Outfit = Outfit({
+  subsets: ["latin"], weight: ["400", "500", "600", "700"]
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const Ovo = Ovo({
+  subsets: ["latin"], weight: ["400"]
 });
 
 export const metadata = {
@@ -20,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${Outfit.className} ${Ovo.className} antialiased`}
       >
         {children}
       </body>
