ada24d7a6446081e1c5118ba21b735b1a828c8c0
mm-pro <robert.codingpro@gmail.com>
2025-02-10 12:47:40 +0800

    config assets.js

---
+++ assets/assets.js
@@ -0,0 +1,37 @@
+
+
+
+ export const assets = { };
+
+ export const workData = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+
+];
+
+ export const serviceData = [
+    { },
+    { },
+    { },
+    { },
+];
+
+ export const infoList = [
+    {},
+    {},
+    {},
+ ];
+
+ export const toolsData = [
+    { }
+];
+
