7477c96eac3c97da6bdeae624e79dc476d8d183d
mm-pro <robert.codingpro@gmail.com>
2025-02-23 04:08:38 +0800

    change:isScroll:bg-opacity 80

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
   return (
     <>
      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50
-        ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}>
+        ${isScroll ? "bg-white bg-opacity-80 backdrop-blur-lg shadow-md" : ""}`}>
         <a href="#top" className='flex px-10'>
             <Image src={assets.logo} alt='' className='w-12 cursor-pointer' />
         </a>
