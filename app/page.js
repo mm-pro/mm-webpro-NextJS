3148ac3fb4744e2190dfe01e46c1a2d6929f665c
mm-pro <robert.codingpro@gmail.com>
2025-02-28 21:00:07 +0800

    Add metadata

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 'use client';
+import Head from 'next/head'
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
@@ -11,6 +12,20 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <>
+    <Head>
+        <title>Dan | Full-Stack Developer</title>
+        <meta name="description" content="Welcome to my developer portfolio. I specialize in web development, AI integration." />
+        <meta name="keywords" content="developer, portfolio, web development, Back-end Developer, AI Integration, software engineer" />
+        <meta name="author" content="Dan" />
+        <meta property="og:title" content="Dan | Full-stack Developer" />
+        <meta property="og:description" content="Explore my projects and skills as a web developer." />
+        <meta property="og:image" content="/public/Dinh.jpg" />
+        <meta property="og:url" content="https://mm-pro.netlify.app" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Dan | Full-stack Developer" />
+        <meta name="twitter:description" content="Check out my work and expertise in Software development." />
+        <meta name="twitter:image" content="/public/Dinh.jpg" />
+    </Head> 
     <Navbar />
     <Header />
     <About />
