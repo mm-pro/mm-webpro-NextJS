bcfd9b5aabcc93cf33cede2292ad2c4d33798001
mm-pro <robert.codingpro@gmail.com>
2025-03-14 20:15:12 +0900

    fix : services_1

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -6,14 +6,14 @@ const Services = () => {
   return (
     <div id='services' className='w-full px-[10%] lg:px-[15%] py-10 scroll-mt-20'>
       <h4 className='text-center mb-2 text-2xl font-Ovo relative'>What I Offer</h4>
-      <h2 className='text-center text-5xl font-Ovo'>My Services</h2>
+      <h2 className='text-center text-5xl font-Ovo mb-4'>My Services</h2>
 
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 my-4'>
         {serviceData.map(({icon, title, description}, index)=>(
           <div key={index} className='border border-gray-400 rounded-lg p-8 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black'>
             <Image src={icon} alt='title' className='w-8'/>
             <h3 className='text-lg py-2 text-gray-700'>{title}</h3>
-            <p className='text-gray-600 leading-5'>{description}</p>
+            <p className='text-gray-600 leading-normal'>{description}</p>
           </div>
         ))}
       </div>
